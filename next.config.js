f65e36afdc4374f098903c9bb2a96cc616c338c9
Hammed <servertstng@gmail.com>
2024-12-24 13:52:38 +0100

    Online URL

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    // URL: 'https://www.scentsbyhoppey.com/',
-    URL: 'http://127.0.0.1:8888/',
+    URL: 'https://www.scentsbyhoppey.com/',
+    // URL: 'http://127.0.0.1:8888/',
     SECRET_KEY: 'kHbSN.%_&Pf4D}(V9-}X2,?23K0m=U5zGe#7yhLe6(dQtxi%R{92zC1tya31JTB5Lbf*b?h.G;L7#KGML?*rc)H,E?3F%F=N/Hg}',
     ENV: 'development'
   },
