bc0c5984ea8a5a747403a1b4755a28e8fde05828
Hammed <servertstng@gmail.com>
2025-01-22 13:05:41 +0100

    ClassRoom - 2

--- app/(home)/blog/page.tsx
+++ app/(home)/blog/page.tsx
@@ -1,36 +1,18 @@
-'use client'
- 
-import { useEffect } from 'react'
-import { PuffLoader } from 'react-spinners'
- 
-export default function Error({
-  error
-}: {
-  error: Error & { digest?: string }
-}) {
-  useEffect(() => {
-    console.error(error)
-  }, [error])
- 
+"use client"
+
+
+export default function Blog() 
+{
+
   return (
-    <div 
-      className="col-span-12 bg-green-400 flex d-flex bg-green-50 border-shadow drop-shadow-lg md:block px-3 md:px-10 py-5 mt-3 rounded-2xl md: mb-0 h-[700px] justify-center item-center" 
-      style={{ marginTop: '0px', paddingTop: '50px' }}
-    >
-        <div 
-          className="container flex justify-center items-center ml-20"
-          style={{ marginTop: '250px' }}
-        >
-            <PuffLoader color="#1c9236" />
-            {/* <h2>Something went wrong!</h2>
-            <button
-              onClick={
-                () => reset()
-              }
-            >
-              Try again +
-            </button> */}
-        </div>
-    </div>
+            <div 
+                  className='container md:grid md:grid-cols-12 justify-between mt-7 mb-4 mx-auto'
+            > 
+                    <h1 
+                        className='font-bold'
+                    >
+                        Blog
+                    </h1>
+            </div>
   )
 }
